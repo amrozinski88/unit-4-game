39eb131e1edd87f5d4bc2608d133df5aac8eb13b
AdamM <ajmrozinski@gmail.com>
2019-07-03 05:00:04 -0400

    all jscript added still working on styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,82 @@
 // random number between 19-120
 // crystals 1-12
-console.log("im working")
+var wins = 0;
+var losses = 0;
+var currentNum = 0;
+var goalNum = 0;
+var crystalValues = [];
+
+goalNum = getRandomNumber(101)+19;
+
+function reset() {
+    currentNum = 0;
+    crystalValues = [];
+    crystalAssigner(); 
+    goalNum = getRandomNumber(101)+19
+    $("#goalNumber").text(goalNum);
+
+
+}
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function crystalAssigner() {
+    for (var i = 0; i < 4; i++) {
+        var crystalValue = getRandomNumber(11) + 1;
+        while (crystalValues.includes(crystalValue)) {
+            crystalValue = getRandomNumber(11) + 1;
+        }
+        crystalValues.push(crystalValue);
+
+    }
+}
+function winOrLose (){
+    if (currentNum === goalNum){
+        alert("you win!");
+        wins+=1;
+        $("#wins").text(wins);
+        reset();
+    }
+    else if (currentNum > goalNum){
+        alert("You Lose!");
+        losses+=1;
+        $("#losses").text(losses)
+        reset();
+    }
+
+    
+}
+$("#gem1").on("click",function(){
+    currentNum = currentNum + crystalValues[0];
+    $("#currentNumber").text(currentNum);
+    winOrLose();
+    
+})
+$("#gem2").on("click",function(){
+    currentNum = currentNum + crystalValues[1];
+    $("#currentNumber").text(currentNum);
+    winOrLose();
+
+})
+$("#gem3").on("click",function(){
+    currentNum = currentNum + crystalValues[2];
+    $("#currentNumber").text(currentNum);
+    winOrLose();
+
+})
+$("#gem4").on("click",function(){
+    currentNum = currentNum + crystalValues[3];
+    $("#currentNumber").text(currentNum);
+    winOrLose();
+
+})
+
+
+
+
+reset();
+
+
+
